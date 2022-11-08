bbe099f8156aa5a1a7cb9616819d60042218a330
Manasvi Kumar <106152916+manu-798@users.noreply.github.com>
2022-11-08 17:28:24 +0530

    Update employee.py

--- employee.py
+++ employee.py
@@ -1,26 +1,40 @@
-import mysql.connector
-
-def create_new_user(con,name,password,database,permissions):
+def create_new_users(con,name,password,database,permissions,status):
     cursorObject = con.cursor()
     cursorObject.execute('select current_user()')
     r=cursorObject.fetchall()
+    if permissions==0:
+        permissions='SELECT,UPDATE, CREATE'
+    if permissions==1:
+        permissions='*'    
     if r[0][0] == "root@localhost":
-        cursorObject.execute(f"CREATE USER IF NOT EXISTS {name}@localhost IDENTIFIED BY {password}") 
+        cursorObject.execute(f"CREATE USER IF NOT EXISTS {name}@localhost IDENTIFIED BY '{password}'") 
         con.commit()
-        cursorObject.execute(f"GRANT {permissions} ON {database}. * TO @localhost")
+        cursorObject.execute(f"GRANT {permissions} ON {database}.* TO '{name}'@localhost")
         con.commit()
         cursorObject.execute("FLUSH PRIVILEGES")
         con.commit()
     else:
-        print("Access Denied")
+        status.set("Access Denied")
 
-def delete_user(con,name):
+def delete_users(con,name,status):
     cursorObject = con.cursor()
+    cursorObject.execute('select user from mysql.user')
+    r=cursorObject.fetchall()
+    print(r)
     cursorObject.execute('select current_user()')
     r=cursorObject.fetchall()
     if r[0][0] == "root@localhost":
         host="localhost"
         cursorObject.execute(f"DROP user {name}@{host}")
         con.commit()
+        status.set("User Dropped")
     else:
-        print("Access Denied")
+        status.set("Access Denied, only Root could.")
+
+def modify_users():
+    pass
+def view_users(con):
+    cursorObject = con.cursor()
+    cursorObject.execute('select user from mysql.user')
+    r=cursorObject.fetchall()
+    print(r)
